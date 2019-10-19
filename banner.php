6db457a7d20a0a92aaf93ce35d58ed9044d13a5f
Valentin LECART <valecart@student.42.fr>
2019-10-19 11:17:51 +0200

    base structure

---
+++ banner.php
@@ -0,0 +1,24 @@
+<section class="banner_part">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-large-12">
+                <div class="single_banner_slider">
+                    <div class="row">
+                        <div class="col-large-5 col-medium-8">
+                            <div class="banner_text">
+                                <div class="banner_text_iner">
+                                    <h1>Un canapé 100% confort</h1>
+                                    <p>Excellent pour rester poser chez soi, il revolutionnera votre quotidien. Seul ou à plusieurs, c'est incontestablement un must-have pour passer de confortables moments.</p>
+                                    <a href="#" class="btn_2">buy now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="banner_img">
+                            <img src="img/banner_img.png" alt="">
+                        </div>
+                    </div>
+                </div>     
+            </div>
+        </div>
+    </div>
+</section>
