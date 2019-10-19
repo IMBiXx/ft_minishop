ba71ccc7a2d8c627e9fcc1b81e69eb2a371b326c
Valentin LECART <valecart@student.42.fr>
2019-10-19 12:21:48 +0200

    placeholder menu links

--- header.php
+++ header.php
@@ -10,32 +10,32 @@
                                 <a class="nav-link" href="index.html">Accueil</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produits</a>
+                                <a class="nav-link dropdown-toggle" href="#.html" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produits</a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
-                                    <a class="dropdown-item" href="category.html"> shop category</a>
-                                    <a class="dropdown-item" href="single-product.html">product details</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
                                 </div>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
+                                <a class="nav-link dropdown-toggle" href="#.html" id="navbarDropdown_3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="login.html"> login</a>
-                                    <a class="dropdown-item" href="tracking.html">tracking</a>
-                                    <a class="dropdown-item" href="checkout.html">product checkout</a>
-                                    <a class="dropdown-item" href="cart.html">shopping cart</a>
-                                    <a class="dropdown-item" href="confirmation.html">confirmation</a>
-                                    <a class="dropdown-item" href="elements.html">elements</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
                                 </div>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
+                                <a class="nav-link dropdown-toggle" href="#.html" id="navbarDropdown_2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="blog.html"> blog</a>
-                                    <a class="dropdown-item" href="single-blog.html">Single blog</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
+                                    <a class="dropdown-item" href="#.html">menu item</a>
                                 </div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="contact.html">Contact</a>
+                                <a class="nav-link" href="#.html">Contact</a>
                             </li>
                         </ul>
                     </div>
