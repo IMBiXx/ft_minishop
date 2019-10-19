09349edbed7e9f967dd65bc3f951b9fb072ff3de
Valentin LECART <valecart@student.42.fr>
2019-10-19 12:19:25 +0200

    add footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer_part">
+    <div class="copyright_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-large-8">
+                    <div class="copyright_text">
+                        <p>©<?php echo date("Y",time());?> Réalisé par <a target="_newblank" href="https://profile.intra.42.fr/users/valecart">valecart</a> & <a target="_newblank" href="https://profile.intra.42.fr/users/thboura">thboura</a>, 42 student | Piscine PHP Oct 2019.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
